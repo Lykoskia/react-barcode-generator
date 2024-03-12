1d13127a0618cfd9cc19b97ce4334ad631f619be
Lykoskia <68440961+Lykoskia@users.noreply.github.com>
2024-03-12 15:13:04 +0100

    value prop

--- src/AmountInput.js
+++ src/AmountInput.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { NumericFormat } from 'react-number-format';
 
-export default function AmountInput({ handleValueChange, visited, errors, handleBlur }) {
+export default function AmountInput({ value, handleValueChange, visited, errors, handleBlur }) {
   
   const onValueChange = (values) => {
       const { formattedValue, value } = values;
