636505d2a51b5265b788df6e5fe0bc19c5fe8a9a
Lykoskia <68440961+Lykoskia@users.noreply.github.com>
2024-03-12 14:51:44 +0100

    UI issues with croatian number formatting

--- src/AmountInput.js
+++ src/AmountInput.js
@@ -4,33 +4,21 @@ import { NumericFormat } from 'react-number-format';
 export default function AmountInput({ handleValueChange, visited, errors, handleBlur }) {
   
   const onValueChange = (values) => {
-    const { formattedValue, value } = values;
-    const numericValue = value ? parseFloat(value) : 0;
-
-    handleValueChange('amount', numericValue, formattedValue);
+      const { formattedValue, value } = values;
+      handleValueChange('amount', value, formattedValue);
   };
 
-  const croatianNumberFormatter = (value) => {
-    const numericValue = parseFloat(value.replace(/\./g, '').replace(',', '.'));
-    if (isNaN(numericValue)) return "";
-
-    return numericValue
-      .toFixed(2)
-      .replace('.', ',')
-      .replace(/\B(?=(\d{3})+(?!\d))/g, '.');
-  };
-
-  return (
-    <NumericFormat
-      thousandSeparator="."
-      decimalSeparator=","
-      isNumericString
-      onValueChange={onValueChange}
-      format={croatianNumberFormatter}
-      onBlur={() => handleBlur('amount')}
-      className={visited['amount'] ? (errors.amount === '' ? 'valid' : 'invalid') : 'unvisited'}
-      placeholder="max 999.999,99"
-      maxLength={10}
-    />
-  );
+    return (
+        <NumericFormat
+            thousandSeparator="."
+            decimalSeparator=","
+            isNumericString
+            onValueChange={onValueChange}
+            format={croatianNumberFormatter}
+            onBlur={() => handleBlur('amount')}
+            className={visited['amount'] ? (errors.amount === '' ? 'valid' : 'invalid') : 'unvisited'}
+            placeholder="max 999.999,99"
+            maxLength={10}
+        />
+    );
 }
