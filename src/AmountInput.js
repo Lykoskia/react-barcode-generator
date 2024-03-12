9e8f6b657272b053c913cfcc01ceab7627d6ea85
Lykoskia <68440961+Lykoskia@users.noreply.github.com>
2024-03-12 15:26:55 +0100

    Update AmountInput.js

--- src/AmountInput.js
+++ src/AmountInput.js
@@ -3,10 +3,10 @@ import { NumericFormat } from 'react-number-format';
 
 export default function AmountInput({ value, handleValueChange, visited, errors, handleBlur }) {
   
-  const onValueChange = (values) => {
-      const { formattedValue, value } = values;
-      handleValueChange('amount', value, formattedValue);
-  };
+    const onValueChange = (values) => {
+        const { formattedValue, floatValue } = values;
+        handleAmountChange(floatValue, formattedValue);
+    };
 
     return (
         <NumericFormat
