76baec46f7b33a6a226dd277969b6ba4faba858c
Lykoskia <68440961+Lykoskia@users.noreply.github.com>
2024-08-01 09:26:29 +0200

    Update AmountInput.js

--- src/AmountInput.js
+++ src/AmountInput.js
@@ -8,6 +8,14 @@ export default function AmountInput({ value, handleValueChange, visited, errors,
         handleValueChange(value);
     };
 
+    const handleAmountBlur = () => {
+        const regex = /^\d{1,3}(\.\d{3})*,\d{2}$/;
+        if (!regex.test(value)) {
+            handleValueChange('');
+        }
+        handleBlur('amount');
+    };
+
     return (
         <NumericFormat
             thousandSeparator="."
@@ -22,7 +30,7 @@ export default function AmountInput({ value, handleValueChange, visited, errors,
                     handleValueChange(formattedValue);
                 }
             }}
-            onBlur={() => handleBlur('amount')}
+            onBlur={handleAmountBlur}
             className={visited['amount'] ? (errors.amount === '' ? 'valid' : 'invalid') : 'unvisited'}
             placeholder="max 999.999,99"
             maxLength={10}
